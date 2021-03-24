0e3405e942a4765f5ecd5546da17dff833b3d260
quodjp <joaophd2014@gmail.com>
2021-03-24 20:04:27 -0300

    feat: database deploy structure

---
+++ database/sisata.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE sisata IF NOT EXISTS
+
+USE sisata
+
+
+/* ENGLISH
+    Creating tables referent to the users sign up feature 
+*/
+
+/* BRAZILIAN PORTUGUESE
+    asdf
+*/
+
+CREATE TABLE usuario (
+
+);
+
+CREATE TABLE usuario (
+    
+);
