846ee0a6c2b852ac6914bb4ec8c0bae51555eab0
Denis Lima <denislima99@gmail.com>
2021-03-28 03:11:19 -0300

    Add - Exemplo de geração da ATA em PDF

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import { Button } from "@material-ui/core";
 import "./App.css";
 import AtaTemplate from "./components/AtaModel/AtaTemplate";
 
 function App() {
   return (
     <div className="App">
+      <Button
+        color="primary"
+        variant="contained"
+        className="no-print"
+        style={{ marginTop: 100 }}
+        onClick={() => window.print()}
+      >
+        Gerar PDF da ATA
+      </Button>
       <AtaTemplate />
     </div>
   );
