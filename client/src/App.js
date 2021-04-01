ef93ad6dbafd5505a8f844f32948cf59c117b621
Denis Lima <denislima99@gmail.com>
2021-04-01 01:28:33 -0300

    Add - react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AtaHeader from "./components/CreateAta/AtaHeader";
+
 function App() {
-  return <div className="App"></div>;
+  const ata = {
+    id: "01/21",
+  };
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/create">
+            <AtaHeader ata={ata} />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
