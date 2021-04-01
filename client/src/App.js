adc9c182c3dfc28e905ff2e7176791b539e3679d
Denis Lima <denislima99@gmail.com>
2021-03-31 21:08:35 -0300

    Add - pallete color, background color

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,5 @@
-import { Button } from "@material-ui/core";
-import "./App.css";
-import AtaTemplate from "./components/AtaModel/AtaTemplate";
-
 function App() {
-  return (
-    <div className="App">
-      <Button
-        color="primary"
-        variant="contained"
-        className="no-print"
-        style={{ marginTop: 100 }}
-        onClick={() => window.print()}
-      >
-        Gerar PDF da ATA
-      </Button>
-      <AtaTemplate />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#3379FA",
+      contrastText: "#FFF",
+    },
+    secondary: {
+      main: "#26BAF4",
+      contrastText: "#FFF",
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
