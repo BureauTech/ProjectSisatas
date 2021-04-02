544cbff78400c5c0924e4e0c62d68642d61e6aa2
Beatriz Coutinho <beatrizmcout@gmail.com>
2021-04-02 19:53:56 -0300

    menu na vertical

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -12,30 +12,47 @@ import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
 
 
 const styles = (theme) => ({
-    grid: {
+    container: {
         backgroundColor: theme.palette.primary.main,
+        flexDirection: 'column',
+        width: '8%',
+        height: '100%',
+        alignItems: 'center',
+        padding: 8,
     },
     icons: {
         color: theme.palette.primary.contrastText,
         width: '90px',
         height: '90px',
-    }
+    },
 })
 class Menu extends Component {
     render() {
         const { classes } = this.props;
         return (
-            <>
-                <Grid className={classes.grid}>
+            <Grid container className={classes.container}>
+                <Grid item>
                     <NotificationsNoneOutlinedIcon className={classes.icons} />
+                </Grid>
+                <Grid item>
                     <PostAddOutlinedIcon className={classes.icons} />
+                </Grid>
+                <Grid item>
                     <AssignmentOutlinedIcon className={classes.icons} />
+                </Grid>
+                <Grid item>
                     <AssessmentOutlinedIcon className={classes.icons} />
+                </Grid>
+                <Grid item>
                     <AccountCircleOutlinedIcon className={classes.icons} />
+                </Grid>
+                <Grid item>
                     <PeopleAltOutlinedIcon className={classes.icons} />
+                </Grid>
+                <Grid item>
                     <ExitToAppOutlinedIcon className={classes.icons} />
                 </Grid>
-            </>
+            </Grid>
         )
     }
 }
