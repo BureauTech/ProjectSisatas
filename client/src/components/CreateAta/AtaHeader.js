9e6f9ea575da4a9d752156e000df29ca690dc37e
Denis Lima <denislima99@gmail.com>
2021-04-06 20:20:02 -0300

    Separando e importando os estilos usados em mais de um componente

--- client/src/components/CreateAta/AtaHeader.js
+++ client/src/components/CreateAta/AtaHeader.js
@@ -8,45 +8,9 @@ import {
 } from "@material-ui/core";
 import { Component } from "react";
 import "./Components.css";
+import { styles } from "../../assets/styles/Styles";
 
 // Alterando css de componentes
-const styles = (theme) => ({
-  grid: {
-    backgroundColor: theme.palette.primary.main,
-    borderRadius: "20px",
-    padding: 5,
-  },
-  textField: {
-    backgroundColor: "white",
-    borderRadius: "10px",
-    color: "black",
-    width: "100%",
-    paddingLeft: "0.4rem",
-  },
-  biggerText: {
-    color: "white",
-    fontFamily: "Montserrat",
-    fontSize: "2.5rem",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    lineHeight: "3.0625rem",
-    letterSpacing: "0em",
-    textAlign: "left",
-  },
-  normalText: {
-    color: "white",
-    fontFamily: "Montserrat",
-    fontSize: "1.875rem",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    lineHeight: "2rem",
-    letterSpacing: "0em",
-    textAlign: "left",
-  },
-  rowMargin: {
-    margin: 20,
-  },
-});
 
 class AtaHeader extends Component {
   render() {
