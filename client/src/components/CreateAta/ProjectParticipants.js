d30291f693ad1c303ffff5c5468067f889bf7b5f
Denis Lima <denislima99@gmail.com>
2021-04-06 22:05:35 -0300

    Separando as classes de estilos

--- client/src/components/CreateAta/ProjectParticipants.js
+++ client/src/components/CreateAta/ProjectParticipants.js
@@ -7,46 +7,10 @@ import {
 } from "@material-ui/core";
 import { Component } from "react";
 import "./Components.css";
+import { styles } from "../../assets/styles/Styles";
+import ParticipantsRow from "./ParticipantsRow";
 
 // Alterando css de componentes
-const styles = (theme) => ({
-  grid: {
-    backgroundColor: theme.palette.primary.main,
-    borderRadius: "20px",
-    padding: 15,
-    paddingLeft: 25,
-  },
-  textField: {
-    backgroundColor: "white",
-    borderRadius: "10px",
-    color: "black",
-    width: "100%",
-    paddingLeft: "0.4rem",
-  },
-  biggerText: {
-    color: "white",
-    fontFamily: "Montserrat",
-    fontSize: "2.5rem",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    lineHeight: "3.0625rem",
-    letterSpacing: "0em",
-    textAlign: "left",
-  },
-  normalText: {
-    color: "white",
-    fontFamily: "Montserrat",
-    fontSize: "1.875rem",
-    fontStyle: "normal",
-    fontWeight: "normal",
-    lineHeight: "2rem",
-    letterSpacing: "0em",
-    textAlign: "left",
-  },
-  rowMargin: {
-    margin: 20,
-  },
-});
 
 class ProjectParticipants extends Component {
   render() {
