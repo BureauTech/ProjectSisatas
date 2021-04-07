6080832a7fb5e70165149340b75a8eb977238498
Beatriz Coutinho <beatrizmcout@gmail.com>
2021-04-07 09:15:23 -0300

    adicionado rotas e alterado cor dos links do menu

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
           <Route path="/create">
             <AtaHeader ata={ata} />
           </Route>
-          <Route path="/menu">
+          <Route path="/">
             <Menu />
           </Route>
         </Switch>
--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from '@material-ui/core';
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
@@ -9,13 +10,11 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-
 import MenuIcon from '@material-ui/icons/Menu';
 import NotificationsNoneOutlinedIcon from '@material-ui/icons/NotificationsNoneOutlined';
 import PostAddOutlinedIcon from '@material-ui/icons/PostAddOutlined';
@@ -24,6 +23,8 @@ import AssessmentOutlinedIcon from '@material-ui/icons/AssessmentOutlined';
 import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
 import PeopleAltOutlinedIcon from '@material-ui/icons/PeopleAltOutlined';
 import ExitToAppOutlinedIcon from '@material-ui/icons/ExitToAppOutlined';
+
+import '../Menu/Menu.css';
 
 const drawerWidth = 270;
 
@@ -94,6 +95,10 @@ const useStyles = makeStyles((theme) => ({
     },
     text: {
         marginLeft: '7px',
+        color: "#6D6D6D",
+        '&:hover': {
+            textDecoration: 'none',
+        }
     }
 
 }));
@@ -110,6 +115,10 @@ export default function Menu() {
     const handleDrawerClose = () => {
         setOpen(false);
     };
+
+    const activeIcon = () => {
+
+    }
 
     return (
         <div className={classes.root}>
@@ -157,34 +166,62 @@ export default function Menu() {
                 </div>
                 <Divider />
                 <List>
-                    <ListItem button key='home'>
-                        <ListItemIcon ><NotificationsNoneOutlinedIcon className={classes.icons} /></ListItemIcon>
-                        <ListItemText primary='Home/Atualizações' className={classes.text} />
-                    </ListItem>
-                    <ListItem button key='new-record'>
-                        <ListItemIcon><PostAddOutlinedIcon className={classes.icons} /></ListItemIcon>
-                        <ListItemText primary='Nova Ata' className={classes.text} />
-                    </ListItem>
-                    <ListItem button key='show-records'>
-                        <ListItemIcon><AssignmentOutlinedIcon className={classes.icons} /></ListItemIcon>
-                        <ListItemText primary='Exibir Atas' className={classes.text} />
-                    </ListItem>
-                    <ListItem button key='reports'>
-                        <ListItemIcon><AssessmentOutlinedIcon className={classes.icons} /></ListItemIcon>
-                        <ListItemText primary='Relatórios' className={classes.text} />
-                    </ListItem>
-                    <ListItem button key='profile'>
-                        <ListItemIcon><AccountCircleOutlinedIcon className={classes.icons} /></ListItemIcon>
-                        <ListItemText primary='Perfil de Usuário' className={classes.text} />
-                    </ListItem>
-                    <ListItem button key='users-list'>
-                        <ListItemIcon><PeopleAltOutlinedIcon className={classes.icons} /></ListItemIcon>
-                        <ListItemText primary='Usuários Cadastrados' className={classes.text} />
-                    </ListItem>
-                    <ListItem button key='exit'>
-                        <ListItemIcon><ExitToAppOutlinedIcon className={classes.icons} /></ListItemIcon>
-                        <ListItemText primary='Sair' className={classes.text} />
-                    </ListItem>
+                    <Typography>
+                        <Link href=''>
+                            <ListItem button key='home'>
+                                <ListItemIcon ><NotificationsNoneOutlinedIcon className={classes.icons} onClick={activeIcon} /></ListItemIcon>
+                                <ListItemText primary='Home/Atualizações' className={classes.text} activeClassName="icon-active" />
+                            </ListItem>
+                        </Link>
+                    </Typography>
+                    <Typography>
+                        <Link href='new-record'>
+                            <ListItem button key='new-record'>
+                                <ListItemIcon><PostAddOutlinedIcon className={classes.icons} /></ListItemIcon>
+                                <ListItemText primary='Nova Ata' className={classes.text} />
+                            </ListItem>
+                        </Link>
+                    </Typography>
+                    <Typography>
+                        <Link href="show-records">
+                            <ListItem button key='show-records'>
+                                <ListItemIcon><AssignmentOutlinedIcon className={classes.icons} /></ListItemIcon>
+                                <ListItemText primary='Exibir Atas' className={classes.text} />
+                            </ListItem>
+                        </Link>
+                    </Typography>
+                    <Typography>
+                        <Link href="reports">
+                            <ListItem button key='reports'>
+                                <ListItemIcon><AssessmentOutlinedIcon className={classes.icons} /></ListItemIcon>
+                                <ListItemText primary='Relatórios' className={classes.text} />
+                            </ListItem>
+                        </Link>
+                    </Typography>
+                    <Typography>
+                        <Link href="profile">
+                            <ListItem button key='profile'>
+                                <ListItemIcon><AccountCircleOutlinedIcon className={classes.icons} /></ListItemIcon>
+                                <ListItemText primary='Perfil de Usuário' className={classes.text} />
+                            </ListItem>
+                        </Link>
+                    </Typography>
+                    <Typography>
+                        <Link href="users-list">
+                            <ListItem button key='users-list'>
+                                <ListItemIcon><PeopleAltOutlinedIcon className={classes.icons} /></ListItemIcon>
+                                <ListItemText primary='Usuários Cadastrados' className={classes.text} />
+                            </ListItem>
+                        </Link>
+                    </Typography>
+                    <Typography>
+                        <Link href="exit">
+                            <ListItem button key='exit'>
+                                <ListItemIcon><ExitToAppOutlinedIcon className={classes.icons} /></ListItemIcon>
+                                <ListItemText primary='Sair' className={classes.text} />
+                            </ListItem>
+                        </Link>
+                    </Typography>
                 </List>
                 <Divider />
             </Drawer>
