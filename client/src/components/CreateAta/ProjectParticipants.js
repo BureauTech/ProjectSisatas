9b86cb9c79997d4686036beb4758e29d6fce26fe
Denis Lima <denislima99@gmail.com>
2021-04-08 09:45:27 -0300

    Transformando em componente funcional

--- client/src/components/CreateAta/ProjectParticipants.js
+++ client/src/components/CreateAta/ProjectParticipants.js
@@ -5,38 +5,33 @@ import {
   Input,
   withStyles,
 } from "@material-ui/core";
-import { Component } from "react";
 import "./Components.css";
 import { styles } from "../../assets/styles/Styles";
 import ParticipantsRow from "./ParticipantsRow";
 
-// Alterando css de componentes
+const ProjectParticipants = (props) => {
+  const { classes } = props;
 
-class ProjectParticipants extends Component {
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <Container>
-        <Grid container className={classes.grid}>
-          <Grid item xs={12}>
-            <Grid
-              container
-              justify="flex-start"
-              alignItems="center"
-              style={{ marginBottom: 30 }}
-            >
-              <FormLabel className={classes.normalText}>Projeto</FormLabel>
-              <Grid item xs={12} sm={5} md={4}>
-                <Input className={classes.textField} disableUnderline />
-              </Grid>
+  return (
+    <Container>
+      <Grid container className={classes.grid}>
+        <Grid item xs={12}>
+          <Grid
+            container
+            justify="flex-start"
+            alignItems="center"
+            style={{ marginBottom: 30 }}
+          >
+            <FormLabel className={classes.normalText}>Projeto</FormLabel>
+            <Grid item xs={12} sm={5} md={4}>
+              <Input className={classes.textField} disableUnderline />
             </Grid>
-            <ParticipantsRow />
           </Grid>
+          <ParticipantsRow />
         </Grid>
-      </Container>
-    );
-  }
-}
+      </Grid>
+    </Container>
+  );
+};
 
 export default withStyles(styles, { withTheme: true })(ProjectParticipants);
