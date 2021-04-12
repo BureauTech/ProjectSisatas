1089fff3b44380f34e52ff8e44ad8dc5173a4ca3
Denis Lima <denislima99@gmail.com>
2021-04-12 19:39:47 -0300

    Adicionando localizade ptBR para o tema

--- client/src/index.js
+++ client/src/index.js
@@ -3,24 +3,28 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { ptBR } from "@material-ui/core/locale";
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#3379FA",
-      contrastText: "#FFF",
+const theme = createMuiTheme(
+  {
+    palette: {
+      primary: {
+        main: "#3379FA",
+        contrastText: "#FFF",
+      },
+      secondary: {
+        main: "#26BAF4",
+        contrastText: "#FFF",
+      },
     },
-    secondary: {
-      main: "#26BAF4",
-      contrastText: "#FFF",
+    typography: {
+      button: {
+        textTransform: "none",
+      },
     },
   },
-  typography: {
-    button: {
-      textTransform: "none",
-    },
-  },
-});
+  ptBR
+);
 
 ReactDOM.render(
   <React.StrictMode>
