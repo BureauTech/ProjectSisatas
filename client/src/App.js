2dbeede8345bc7ce7d1951bde973a05d039d1786
Denis Lima <denislima99@gmail.com>
2021-04-13 08:53:02 -0300

    Colocando rotas e menu para todas as telas

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,33 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Pauta from "./components/CreateAta/Pauta";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Data from "./components/ExibirAta/Data";
 import Register from "./components/RegisterUser/Register";
 import CreateAta from "./pages/CreateAta/CreateAta";
-import AtaHeader from "./components/CreateAta/AtaHeader";
-import Menu from "./components/Menu/Menu"
+import Menu from "./components/Menu/Menu";
+import { Grid } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/registeruser">
-            <Register/>
-          <Route path="/createata">
-            <CreateAta />
-          </Route>
-          <Route path="/">
-            <Menu />
-          </Route>
-        </Switch>
-      </Router>
-      { /* <Data/> */ }
-    </div>
+    <Router>
+      <div className="App">
+        <Menu />
+        <Grid
+          container
+          alignItems="center"
+          style={{ marginTop: 60, marginLeft: 100 }}
+        >
+          <Switch>
+            <Route path="/registeruser">
+              <Register />
+            </Route>
+            <Route path="/new-record">
+              <CreateAta />
+            </Route>
+            <Route path="/"></Route>
+          </Switch>
+        </Grid>
+        {/* <Data/> */}
+      </div>
+    </Router>
   );
 }
 export default App;
