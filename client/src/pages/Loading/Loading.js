d8cea37cd1703c9b34769c6d25785d08d2ba9ca8
Denis Lima <denislima99@gmail.com>
2021-04-18 04:21:59 -0300

    Alterando forma como funciona o Loading (estilos)

--- client/src/pages/Loading/Loading.js
+++ client/src/pages/Loading/Loading.js
@@ -1,12 +1,17 @@
-import { CircularProgress, Container, Grid } from "@material-ui/core";
+import { CircularProgress, Dialog, Grid } from "@material-ui/core";
+import "./Loading.css";
 
 const Loading = () => {
   return (
-    <Container>
-      <Grid container justify="center">
+    <Dialog open className="invisible">
+      <Grid
+        container
+        justify="center"
+        style={{ width: "6rem", height: "6rem" }}
+      >
         <CircularProgress style={{ width: "4rem", height: "4rem" }} />
       </Grid>
-    </Container>
+    </Dialog>
   );
 };
 
