cbe1cd9c45998615655557f0e9b99532f72600cc
Denis Lima <denislima99@gmail.com>
2021-04-30 20:22:19 -0300

    Arrumando layout caso não haja assinatura

--- client/src/components/Ata/AtaModel/Assinaturas.js
+++ client/src/components/Ata/AtaModel/Assinaturas.js
@@ -3,11 +3,11 @@ import { Container, Grid, Typography } from "@material-ui/core";
 const Assinaturas = (props) => {
   const { listaParticipantes } = props.dados;
   return (
-    <Container style={{ marginTop: "3cm", padding: 0 }}>
+    <Container style={{ marginTop: "2cm", padding: 0 }}>
       <Grid container justify="flex-start">
         {listaParticipantes.map((participante) => (
-          <Container style={{ padding: 0 }}>
-            <Grid container justify="flex-start">
+          <Container style={{ padding: 0, marginBottom: "0.7cm" }}>
+            <Grid container justify="flex-start" style={{ marginBottom: 15 }}>
               <Typography>Representante: {participante.area}</Typography>
             </Grid>
             <Grid container justify="flex-start" style={{ paddingTop: 20 }}>
@@ -18,6 +18,7 @@ const Assinaturas = (props) => {
                   style={{ maxHeight: "1.5cm", borderBottom: "1px solid black" }}
                 />
               )}
+              {!participante.assinatura && <div className="space black"></div>}
             </Grid>
             <Grid container justify="flex-start" style={{ marginBottom: 30 }}>
               <Typography>
--- client/src/components/Ata/AtaModel/AtaTemplate.js
+++ client/src/components/Ata/AtaModel/AtaTemplate.js
@@ -1,8 +1,5 @@
 import { Container, Grid, Typography } from "@material-ui/core";
 import logo from "../../../assets/images/iacit-logo.jpg";
-import denisSign from "../../../assets/images/signTest/denis.png";
-import biaSign from "../../../assets/images/signTest/bia.png";
-import jpSign from "../../../assets/images/signTest/jp.png";
 import "./AtaTemplate.css";
 import "./printConfig.css";
 import AtaHeader from "./Header";
@@ -30,78 +27,6 @@ const AtaTemplate = (props) => {
     observacoes: infoAta.observacoes ? infoAta.observacoes : [],
     assuntos: infoAta.assuntos ? infoAta.assuntos : "",
   };
-  // const id = "01/21";
-  // const data = "25/03/2021 - 01/04/2021";
-  // const inicio = "08h00";
-  // const fim = "12h00";
-  // const local = "Google Meet";
-  // const tema = "Gerenciador de ATAS";
-  // const listaParticipantes = [
-  //   {
-  //     nome: "Denis Ferreira Lima",
-  //     cargo: "Desenvolvedor",
-  //     area: "BureauTech",
-  //     email: "denis@bureautech.com",
-  //     telefone: "+55 (12) 991234567",
-  //     assinatura: denisSign,
-  //   },
-  //   {
-  //     nome: "Beatriz Coutinho",
-  //     cargo: "Desenvolvedor",
-  //     area: "BureauTech",
-  //     email: "bia@bureautech.com",
-  //     telefone: "+55 (12) 997654321",
-  //     assinatura: biaSign,
-  //   },
-  //   {
-  //     nome: "João Pedro Santos",
-  //     cargo: "Master",
-  //     area: "BureauTech",
-  //     email: "jp@bureautech.com",
-  //     telefone: "+55 (12) 995746321",
-  //     assinatura: jpSign,
-  //   },
-  // ];
-  // const pauta =
-  //   "Na presente reunião foi requisitado que fosse estudado e desenvolvido um meio de exportar a ATA para PDF";
-  // const observacoes = [
-  //   "Deve ser disponibilizada cópia da Ata de Reunião para os participantes e envolvidos",
-  //   "O campo PRAZO define as datas de entrega das solicitações por parte dos responsáveis definidos no campo RESPONSÁVEL.",
-  // ];
-  // const assuntos = [
-  //   {
-  //     id: 1,
-  //     assunto: "Exportar HTML para PDF",
-  //     responsavel: "Denis Lima",
-  //     prazo: "28/03/2021",
-  //   },
-  //   {
-  //     id: 2,
-  //     assunto: "Auxiliar na criação do HTML",
-  //     responsavel: "Beatriz Coutinho",
-  //     prazo: "28/03/2021",
-  //   },
-  //   {
-  //     id: 3,
-  //     assunto: "Validar e remover impedimentos",
-  //     responsavel: "João Pedro",
-  //     prazo: "28/03/2021",
-  //   },
-  // ];
-
-  // const dados = {
-  //   id: id,
-  //   data: data,
-  //   inicio: inicio,
-  //   fim: fim,
-  //   local: local,
-  //   logo: logo,
-  //   tema: tema,
-  //   listaParticipantes: listaParticipantes,
-  //   pauta: pauta,
-  //   observacoes: observacoes,
-  //   assuntos: assuntos,
-  // };
 
   return (
     <Container>
