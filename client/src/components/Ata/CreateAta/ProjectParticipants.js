7771547113757f9c88198a4e6eb954f5052577f5
Denis Lima <denislima99@gmail.com>
2021-05-07 19:46:46 -0300

    Trocando tipo DataGrid por Participante

--- client/src/components/Ata/CreateAta/ProjectParticipants.js
+++ client/src/components/Ata/CreateAta/ProjectParticipants.js
@@ -37,7 +37,7 @@ const ProjectParticipants = (props) => {
 
   useEffect(() => {
     userServices
-      .listarUsuarios("DataGrid")
+      .listarUsuarios("Participante")
       .then((res) => {
         console.log(res.data);
         setListaParticipantes(res.data);
