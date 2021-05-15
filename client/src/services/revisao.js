1bf04fe2cf6c16439b1e6e125d04004c5aa27f3c
Caique Nascimento <caiquemigueldes@gmail.com>
2021-05-15 15:08:58 -0300

    listar revisoes

--- client/src/services/revisao.js
+++ client/src/services/revisao.js
@@ -1,11 +1,17 @@
 import api from "./api";
+const endpoint = "/revisoes"
 
 const criarRevisao = (data) => {
-  return api.post("/revisoes/cadastrarRevisoes", data);
+  return api.post(`${endpoint}/cadastrarRevisoes`, data);
 };
+
+const listarRevisoes = () => {
+  return api.get(`${endpoint}/listarRevisoes`)
+}
 
 const revisaoServices = {
   criarRevisao,
+  listarRevisoes,
 };
 
 export default revisaoServices;
