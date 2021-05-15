30b52f853ddd8945cfe936c7ce95a07b15b747ba
Daniel Santos Oliveira <daniel.oliveira96@fatec.sp.gov.br>
2021-05-15 17:51:27 -0300

    Atualização: Adicionado método para pegar revições específicas.

--- server/sisata/src/main/java/br/com/iacit/sisatas/repository/RevisoesRepository.java
+++ server/sisata/src/main/java/br/com/iacit/sisatas/repository/RevisoesRepository.java
@@ -2,10 +2,13 @@ package br.com.iacit.sisatas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.com.iacit.sisatas.models.AtasModel;
 import br.com.iacit.sisatas.models.RevisoesModel;
 
 public interface RevisoesRepository extends JpaRepository<RevisoesModel, String> {
 	
 	RevisoesModel findByrevId(long revId);
+	
+	RevisoesModel findBycontemRevisoes(AtasModel Ata);
 	
 }
