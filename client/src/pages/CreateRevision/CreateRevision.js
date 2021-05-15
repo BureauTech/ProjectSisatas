ab6dfd3610f28d303b1dbfea0983c5dbc5b95b78
Caique Nascimento <caiquemigueldes@gmail.com>
2021-05-15 17:39:30 -0300

    conflito

--- client/src/pages/CreateRevision/CreateRevision.js
+++ client/src/pages/CreateRevision/CreateRevision.js
@@ -9,7 +9,6 @@ import "./Style.css";
 import revisaoServices from "../../services/revisao";
 import Alerta from "../../components/Snackbar/Alerta";
 
-
 const CreateRevision = (props) => {
   const theme = useTheme();
 
@@ -19,79 +18,77 @@ const CreateRevision = (props) => {
   const [openSnack, setOpenSnack] = useState(false);
   const [msgSucesso, setMsgSucesso] = useState("");
   const [msgErro, setMsgErro] = useState("");
-  const [disab, setDisab] = useState(false);
 
   const [user, setUser] = useState("");
   const [ataid, setAtaid] = useState("");
 
   const location = useLocation();
 
-
   const history = useHistory();
-
 
   const body = {
     ...infoHeader,
-    revAssunto
-   };
-   //body.infoHeader.contemRevisoes.ataId = props.ataid;
-   //body.responsavelRevisoes.usuId = props.user;
+    revAssunto,
+  };
+  //body.infoHeader.contemRevisoes.ataId = props.ataid;
+  //body.responsavelRevisoes.usuId = props.user;
 
   const CriarRevisao = (e) => {
     e.preventDefault();
-    
 
-    revisaoServices.criarRevisao(body)
-    .then((res) => {
-
-        console.log(JSON.stringify(res));
+    revisaoServices
+      .criarRevisao(body)
+      .then((res) => {
         setMsgSucesso("Revisão cadastrada com sucesso!");
         setMsgErro(false);
         setOpenSnack(true);
-        setDisab(true);
+      })
+      .catch((err) => {
+        console.log(err);
+        setMsgSucesso(false);
+        setMsgErro("Ocorreu um erro ao cadastrar a revisão");
+        setOpenSnack(true);
+      });
 
-        setTimeout(
-          function() {
-            history.push("ata", { id: location.state.ataid });
-        }, 1250)
-    })
-    .catch(err => {
-      console.log(err);
-      setMsgSucesso(false);
-      setMsgErro("Ocorreu um erro ao cadastrar a revisão");
-      setOpenSnack(true);
-    })
-      console.log("olha"+ JSON.stringify(body));
-
+    let seu_tempo_ta_acabando = setTimeout(function () {
+      history.push("ata", { id: location.state.ataid });
+    }, 4000);
   };
-
+  console.log(location.state);
 
   return (
     <Container>
       <form onSubmit={(e) => CriarRevisao(e)}>
         <Grid container style={{ marginBottom: 10 }}>
-          <RevisionHeader setRevHeader={setRevHeader} resp={location.state.user} ataid={location.state.ataid}/>
+          <RevisionHeader
+            setRevHeader={setRevHeader}
+            resp={location.state.user}
+            ataid={location.state.ataid}
+            ataDataInicio={location.state.ataDataInicio}
+            setMsgSucesso={setMsgSucesso}
+            setMsgErro={setMsgErro}
+            setIsOpen={setOpenSnack}
+          />
         </Grid>
         <Grid container style={{ marginBottom: 10 }}>
-          <RevisionSubject setInfoAss={setInfoAss}/>
+          <RevisionSubject setInfoAss={setInfoAss} />
         </Grid>
         <Grid container justify="space-between" style={{ padding: 24 }}>
-            <Button
-              variant="contained"
-              className="bold"
-              style={{
-                backgroundColor: "white",
-                color: theme.palette.secondary.main,
-                fontWeight: 700,
-                fontSize: "1.5rem",
-                borderRadius: 20,
-                padding: "0 30px",
-              }}
-              onClick={() => history.push("ata", { id: location.state.ataid })}
-              
-            >
-              Cancelar
-            </Button>
+          <Button
+            variant="contained"
+            className="bold"
+            style={{
+              backgroundColor: "white",
+              color: theme.palette.secondary.main,
+              fontWeight: 700,
+              fontSize: "1.5rem",
+              borderRadius: 20,
+              padding: "0 30px",
+            }}
+            onClick={() => history.push("ata", { id: location.state.ataid })}
+          >
+            Cancelar
+          </Button>
           <Button
             variant="contained"
             color="secondary"
@@ -103,7 +100,6 @@ const CreateRevision = (props) => {
               borderRadius: 20,
               padding: "0 30px",
             }}
-            disabled={disab}
           >
             Salvar
           </Button>
