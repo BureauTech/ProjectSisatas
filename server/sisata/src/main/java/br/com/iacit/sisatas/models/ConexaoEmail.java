93704b410c7e8a551039f25810947da87b91f096
Daniel Santos Oliveira <daniel.oliveira96@fatec.sp.gov.br>
2021-05-19 09:32:40 -0300

    Updade: Atualização de atributos.

--- server/sisata/src/main/java/br/com/iacit/sisatas/models/ConexaoEmail.java
+++ server/sisata/src/main/java/br/com/iacit/sisatas/models/ConexaoEmail.java
@@ -1,5 +1,12 @@
 package br.com.iacit.sisatas.models;
 
+import java.util.Map;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class ConexaoEmail {
 	public String userEnviar = ""; //usuario (email sem dominio)
 	public String senhaEnviar = ""; //senha do email
@@ -9,6 +16,12 @@ public class ConexaoEmail {
 	public String nomeReceber = ""; //nome de quem vai receber a mensagem
 	public String nomeArq = ""; //nome do arquivo (se for enviar email com anexo)
 	public String arq = ""; //path do arquivo do anexo (se for enviar email com anexo)
-	public String url = ""; //url do arquivo do anexo (se for enviar email com anexo)
+	//public String url = ""; //url do arquivo do anexo (se for enviar email com anexo)
 	
+	public Map<String, Object> props;
+	
+	public String mailTo;
+	public String Subject;
+	public String from;
+
 }
